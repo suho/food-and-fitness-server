88265b6b15e26220558606715c2b17e768c4448f
Su Ho V <hovansu.it@gmail.com>
2017-04-08 15:06:39 +0700

    - add devise token - add carrierwave for upload images

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
+          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   include DeviseTokenAuth::Concerns::User
+  mount_uploader :image, AvatarUploader
 end
